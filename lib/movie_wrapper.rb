c3d5269493363c7b98549594159af16ab21434c7
CheezItMan <chris@adadevelopersacademy.org>
2018-12-17 10:52:25 -0800

    took out logger

--- lib/movie_wrapper.rb
+++ lib/movie_wrapper.rb
@@ -1,4 +1,3 @@
-Rails.logger = Logger.new(STDOUT)
 
 class MovieWrapper
   BASE_URL = "https://api.themoviedb.org/3/"
@@ -10,13 +9,13 @@ class MovieWrapper
 
   def self.search(query)
     url = BASE_URL + "search/movie?api_key=" + KEY + "&query=" + query
-    logger.debug url
+    puts url
     # puts url
     response =  HTTParty.get(url)
     if response["total_results"] == 0
       return []
     else
-      logger.debug response
+      puts response
       movies = response["results"].map do |result|
         self.construct_movie(result)
       end
