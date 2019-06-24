2e10b6143a17df40fd11230779a76a256f73e4cd
Kaida Masaki <kaida@adadevelopersacademy.org>
2019-06-24 14:51:39 -0700

    Add delay and retry requests on failure

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,8 @@ customer_data.each do |customer|
 end
 
 JSON.parse(File.read('db/seeds/movies.json')).each do |movie_data|
+  sleep(0.1)                    # Sleep to avoid hammering the API.
+
   movies = MovieWrapper.search(movie_data["title"])
   ap "#{movie_data['title']} Added to the library!"
   movies.first.inventory = movie_data['inventory']
