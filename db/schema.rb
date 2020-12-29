ba48b23c2f9295021b3074b7fb7b260552041138
Chris McAnally <chris@adadevelopersacademy.org>
2020-12-29 12:42:52 -0800

    update dependencies

--- db/schema.rb
+++ db/schema.rb
@@ -2,8 +2,8 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 2020_07_06_194441) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "customers", id: :serial, force: :cascade do |t|
+  create_table "customers", force: :cascade do |t|
     t.string "name"
     t.datetime "registered_at"
     t.string "address"
@@ -28,7 +28,7 @@ ActiveRecord::Schema.define(version: 2020_07_06_194441) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "rentals", id: :serial, force: :cascade do |t|
+  create_table "rentals", force: :cascade do |t|
     t.integer "customer_id"
     t.integer "video_id"
     t.date "checkout_date"
@@ -40,7 +40,7 @@ ActiveRecord::Schema.define(version: 2020_07_06_194441) do
     t.index ["video_id"], name: "index_rentals_on_video_id"
   end
 
-  create_table "videos", id: :serial, force: :cascade do |t|
+  create_table "videos", force: :cascade do |t|
     t.string "title"
     t.text "overview"
     t.date "release_date"
