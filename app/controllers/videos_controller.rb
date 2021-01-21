a4714a2d7407986e85a47e160333e9b261ec1103
Sophie Messing <sophie.e.messing@gmail.com>
2021-01-20 19:29:24 -0800

    added new route for adding a video from API library

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -21,6 +21,26 @@ class VideosController < ApplicationController
       )
   end
 
+  def create
+      @video = Video.new
+      @video.title = params[:title]
+      @video.overview = params[:overview]
+      @video.release_date = params[:release_date]
+      @video.image_url = "https://image.tmdb.org/t/p/w185" + params[:poster_path]
+      @video.external_id = params[:id]
+      @video.inventory = 5
+      @video.save
+      
+      render(
+      status: :ok,
+      json: @video.as_json(
+        only: [:title, :overview, :release_date, :image_url, :external_id, :inventory],
+        methods: [:available_inventory]
+        )
+      )
+      
+    end
+
   private
 
   def require_video
